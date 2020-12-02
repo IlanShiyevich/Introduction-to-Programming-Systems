2b7a46474434a0f2a29c6dbe06940aa8b0dcb33c
Ubuntu <ubuntu@ip-172-31-41-253.us-east-2.compute.internal>
2020-12-02 12:54:14 +0000

    update

--- myBank.c
+++ myBank.c
@@ -3,74 +3,74 @@
 static int accountNumb = 901;
 
 int newAccount (double firstDeposit) {
-	    int i = 0;
-	        while (bankAccounts[i][0] != 0) {
-			        i++;
-				        if(i==50){
-						            return -999999999;
-							            }
-					    }
-		    bankAccounts[i][1] = firstDeposit;
-		        bankAccounts[i][0] = 1;
-			    return accountNumb+i;
+    int i = 0;
+    while (bankAccounts[i][0] != 0) {
+        i++;
+        if(i==50){
+            return -999999999;
+        }
+    }
+    bankAccounts[i][1] = firstDeposit;
+    bankAccounts[i][0] = 1;
+    return accountNumb+i;
 }
 
 double Balance(int account_number){
-	    if(bankAccounts[account_number - accountNumb][0] == 0 ){
-		            return  -9999999999;
-			        }
-	        return bankAccounts[account_number - accountNumb][1];
+    if(bankAccounts[account_number - accountNumb][0] == 0 ){
+        return  -9999999999;
+    }
+    return bankAccounts[account_number - accountNumb][1];
 }
 
 double Deposit(int account_number,double amount){
-	    if(bankAccounts[account_number - accountNumb][0] == 0 ){
-		            return -9999999999;
-			        }
-	        else if(amount < 0){
-			        return -9999999999.1;
-				    }
-		    return bankAccounts[account_number-accountNumb][1] = bankAccounts[account_number-accountNumb][1] + amount;
+    if(bankAccounts[account_number - accountNumb][0] == 0 ){
+        return -9999999999;
+    }
+    else if(amount < 0){
+        return -9999999999.1;
+    }
+    return bankAccounts[account_number-accountNumb][1] = bankAccounts[account_number-accountNumb][1] + amount;
 }
 
 
 
 double withdrawal(int account_number,double amount){
-	    if(bankAccounts[account_number - accountNumb][0] ==0 ){
-		            return  -9999999999;
-			        }
-	        if(bankAccounts[account_number-accountNumb][1] < amount){
-			        return  -9999999999.2;
-				    }
-		    return bankAccounts[account_number-accountNumb][1] = bankAccounts[account_number-accountNumb][1] - amount;
+    if(bankAccounts[account_number - accountNumb][0] ==0 ){
+        return  -9999999999;
+    }
+    if(bankAccounts[account_number-accountNumb][1] < amount){
+        return  -9999999999.2;
+    }
+    return bankAccounts[account_number-accountNumb][1] = bankAccounts[account_number-accountNumb][1] - amount;
 }
 
 void terminateAccount(int account_number){
-	    if(bankAccounts[account_number - accountNumb][0] == 0 ){
-		            printf("This account is already closed");
-			        }
-	        bankAccounts[account_number - accountNumb][0] = 0;
+    if(bankAccounts[account_number - accountNumb][0] == 0 ){
+        printf("This account is already closed");
+    }
+    bankAccounts[account_number - accountNumb][0] = 0;
 }
 
 void addInterest(double interest_rate){
-	    int i = 0;
-	        while(bankAccounts[i][0] == 1){
-			        bankAccounts[i][1] += (bankAccounts[i][1]*interest_rate)/100;
-					i++;			   
-	       	}
+    int i = 0;
+    while(bankAccounts[i][0] == 1){
+        bankAccounts[i][1] += (bankAccounts[i][1]*interest_rate)/100;
+        i++;
+    }
 }
 
 
 void printAccounts(){
-	    int i = 0;
-	        while(bankAccounts[i][0] != 0){
-			        printf("The balance of account number %d is: %.2lf", accountNumb+i, bankAccounts[i][1]);
-				        i++;
-					    }
+    int i = 0;
+    while(bankAccounts[i][0] != 0){
+        printf("The balance of account number %d is: %.2lf", accountNumb+i, bankAccounts[i][1]);
+        i++;
+    }
 }
 
 void Exit(){
-	    for(int i=0;i<50;i++){
-		            bankAccounts[i][0] = 0;
-			        }
+    for(int i=0;i<50;i++){
+        bankAccounts[i][0] = 0;
+    }
 }
 
