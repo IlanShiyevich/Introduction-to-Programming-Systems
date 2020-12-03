c8d5648f072f50bbe4a3e7b1c153273be107dac8
IlanShiyevich <73855961+IlanShiyevich@users.noreply.github.com>
2020-12-04 00:22:22 +0200

    small main fix

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main() {
                " C-Close Account\n"
                " I-Interest\n"
                " P-Print\n"
-               " E-Exit \n");
+               " E-Exit\n");
         scanf(" %c", &option);
         switch(option){
 
@@ -125,5 +125,3 @@ int main() {
         }
     }
 }
-
-
